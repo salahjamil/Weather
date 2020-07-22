20234c435c9850361f73d752f1676f1fb656b584
Salah Tomezy <salahidna@gmail.com>
2020-07-22 20:15:49 +0300

    Updates

---
+++ precache-manifest.e36d9237029264f6e118fabdc7115ad4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1dadde5c1666b0dfe44a5b5aa4a1017",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "53ab0e9cdddf786dfa88",
+    "url": "/Weather/static/css/main.21c419e6.chunk.css"
+  },
+  {
+    "revision": "7cf2182656a62ed6117a",
+    "url": "/Weather/static/js/2.d437b892.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather/static/js/2.d437b892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53ab0e9cdddf786dfa88",
+    "url": "/Weather/static/js/main.eddbcc9e.chunk.js"
+  },
+  {
+    "revision": "33b8acc232781a3d41c0",
+    "url": "/Weather/static/js/runtime-main.22be1482.js"
+  },
+  {
+    "revision": "ad11bb371c03b8cc30d52a890307e674",
+    "url": "/Weather/static/media/bg.ad11bb37.jpg"
+  }
+]);
